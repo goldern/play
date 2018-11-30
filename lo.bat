971b16feaf9b8cda5ca69031135df7312fceb842
Goldern <golderng1@gmail.com>
2018-11-30 14:27:43 -0300

    first commit

---
+++ lo.bat
@@ -0,0 +1,3 @@
+@echo off
+echo  OOO  BBBB  RRRR  IIIII GGGGG AAAAA DDDD  OOOOOO !!! & echo O   O B   B R   R   I   G     A   A D   D O    O !!! & echo O   O BBBB  RRRR    I   G  GG AAAAA D   D O    O !!! & echo O   O B   B R   R   I   G   G A   A D   D O    O & echo  OOO  B   B R   R IIIII GGGGG A   A DDDD  OOOOOO !!!
+pause
