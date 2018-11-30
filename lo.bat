0d2779cee5cac700e271db8f1919102b65713bee
Goldern <golderng1@gmail.com>
2018-11-30 16:44:43 -0300

    terceiro commit

--- lo.bat
+++ lo.bat
@@ -1,5 +1,9 @@
 @echo off
+echo ""
+echo ""
 echo  OOO  BBBB  RRRR  IIIII GGGGG AAAAA DDDD  OOOOOO !!! & echo O   O B   B R   R   I   G     A   A D   D O    O !!! & echo O   O BBBB  RRRR    I   G  GG AAAAA D   D O    O !!! & echo O   O B   B R   R   I   G   G A   A D   D O    O & echo  OOO  B   B R   R IIIII GGGGG A   A DDDD  OOOOOO !!!
+echo ""
+echo ""
 pause
 cd ..
 rmdir play /S
